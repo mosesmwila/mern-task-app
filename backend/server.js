a5603cdc3a93c637e295d0c487da0f098d46d760
imosesmwila <moseshb17@gmail.com>
2024-03-07 01:20:21 +0200

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv").config()
 const express = require("express")
 const connectDB = require("./config/connectDB")
+const mongoose = require("mongoose") 
 
 const app = express()
 
@@ -13,15 +14,23 @@ app.get("/", (req,res) => {
 
 const PORT = process.env.PORT || 5000
 
-
-const startServer = async () => {
-    try {
-        await connectDB();
+mongoose
+     .connect(process.env.MONGO_URI)
+     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         });
-    } catch (error) {
-        console.log(error)
-    }
-};
-startServer();
+     })
+     .catch((err) => console.log(err));
+
+// const startServer = async () => {
+//     try {
+//         await connectDB();
+//         app.listen(PORT, () => {
+//             console.log(`Server running on port ${PORT}`);
+//         });
+//     } catch (error) {
+//         console.log(error)
+//     }
+// };
+// startServer();
