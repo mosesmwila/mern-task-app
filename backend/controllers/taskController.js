8600620e7bdee3f1bc6b6d51bc4fd1ee176d35eb
imosesmwila <moseshb17@gmail.com>
2024-03-10 19:55:30 +0200

    Added Get Single Task and Delete Task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -20,8 +20,39 @@ const getTasks = async (req, res) => {
       }
     };
 
+// Get A Single Task
+const getTask = async(req,res) => {
+    try {
+        const {id} = req.params;
+        const task = await Task.findById(id);
+        if (!task) {
+            return res.status(404).json(`No Task with id: ${id}`);
+        }
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+};
+
+// Delete a task
+const deleteTask = async (req,res) => {
+ try {
+    const {id} = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+        return res.status(404).json(`No Task with id: ${id}`);
+    }
+    
+    res.status(200).send("Task Deleted");
+
+ } catch (error) {
+    res.status(500).json({msg: error.message})
+ }
+};
 
 module.exports = {
     createTask,
     getTasks,
+    getTask,
+    deleteTask,
 };
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const Task = require("../models/taskModel");
 const router = express.Router();
-const {createTask, getTasks} = require("../controllers/taskController");
+const {createTask, getTasks, getTask, deleteTask} = require("../controllers/taskController");
 
 // Api Routes
 router.post("/api/tasks",createTask);
 router.get("/api/tasks",getTasks);
+router.get("/api/tasks/:id", getTask);
+router.delete("/api/tasks/:id",deleteTask);
 
 module.exports = router;
